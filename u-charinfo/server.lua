5a4d85a364fe770bec9f3f24dd3da49e428b5fb6
Uggish Resources <104546122+Uggish@users.noreply.github.com>
2023-07-31 20:26:19 +0200

    added /jobinfo so you can see information about your character's job. - server.lua

--- u-charinfo/server.lua
+++ u-charinfo/server.lua
@@ -18,4 +18,18 @@ end)
 end)
  ]]
 
+ -- English
+ QBCore.Commands.Add('jobinfo', 'Character - Job Info!', {}, false, function(source)
+  local src = source
+  local Player = QBCore.Functions.GetPlayer(src)
+  TriggerClientEvent("QBCore:Notify", source, "Job: "..Player.PlayerData.job.name..", ".."Job Level: "..Player.PlayerData.job.grade.level..", ".."Job Grade Name: "..Player.PlayerData.job.grade.name)
+end)
 
+
+-- Swedish
+--[[ QBCore.Commands.Add('jobinfo', 'Character - Job Info!', {}, false, function(source)
+  local src = source
+  local Player = QBCore.Functions.GetPlayer(src)
+  TriggerClientEvent("QBCore:Notify", source, "Jobb: "..Player.PlayerData.job.name..", ".."Jobb Level: "..Player.PlayerData.job.grade.level..", ".."Jobb Grad Namn: "..Player.PlayerData.job.grade.name)
+end)
+ ]]
